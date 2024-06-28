662e35ff389805768f9ab92df589c7f58457c153
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-06-28 11:21:47 -0300

    (wip): Interface for reading, saving entities on database applying business logic.

---
+++ VocabularyBuilder.App/Interfaces/IKindleFileService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace VocabularyBuilder.App.Interfaces
+{
+    public interface IKindleFileService
+    {
+        Task ReadFile(IFormFile kindleFile);
+        Task SaveBook(List<String> books);
+        Task SaveWord(List<String> words);
+    }
+}
